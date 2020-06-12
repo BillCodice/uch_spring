1362cedfb939fc0b350ad11a80c858c1b179e915
BillCodice <milag@LAPTOP-8L5P84DK.lan>
2020-06-11 19:05:20 -0500

    Mi primer commit

---
+++ src/main/java/com/redsocial/DefSofSemana08Application.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefSofSemana08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefSofSemana08Application.class, args);
+	}
+
+}
